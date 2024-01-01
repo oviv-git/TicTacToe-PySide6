7f9dc5cf98d4588a48764a9c70f143aebf964dbc
oviv-git <119146460+oviv-git@users.noreply.github.com>
2024-01-01 22:29:03 +0200

    Figuring the UI out

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import sys
 from src.game import TicTacToe, Play
 from src.player import HumanPlayer
 from PySide6 import QtWidgets
-# from PySide6.QtCore import QtWidget
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QFile
+from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtUiTools import QUiLoader
 from ui.ui_widget import Ui_Form
 
 
-class Widget(QWidget, Ui_Form):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("TicTacToe")
+
+
+# class Widget(QWidget, Ui_Form):
+#     def __init__(self):
+#         super().__init__()
+#         self.setupUi(self)
+#         self.setWindowTitle("TicTacToe")
 
 def main():
     # game = TicTacToe()
@@ -20,14 +23,19 @@ def main():
     # player_2 = HumanPlayer()
     # play = Play(game, player_1, player_2)
     
-    app = QtWidgets.QApplication(sys.argv)
+    # app = QtWidgets.QApplication(sys.argv)
 
-    window = Widget()
+    app = QApplication(sys.argv)
+    ui_file = QFile("ui/Widget.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+
+    # window = Widget()
     window.show()
 
     app.exec()
-    # t = TicTacToe() 
-    # print(t)
 
 if __name__ == "__main__":
     main()
